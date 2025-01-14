f54ecd511e1020417bbe978e1300b0ec8f9c801e
yubin012 <pyb2675@naver.com>
2025-01-14 23:50:51 +0900

    Login 구현

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from '../screens/Login';
-import SignupScreen from '../screens/Signup';
+import Login from '../screens/Login';
 import HomeScreen from '../screens/HomeScreen';
+import SignUp from '../screens/SingUpScreen';
+import PostLostPet from '../screens/PostLostPet'; // PostLostPet 화면
 
 type RootStackParamList = {
   Login: undefined;
-  Signup: undefined;
+  SignUp: undefined;
   Home: undefined;
+  PostLostPet: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -17,9 +19,10 @@ const AppNavigator: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="PostLostPet" component={PostLostPet}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
